f70c53c28d5709ce01b7abe087beb654cc4cf57c
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2024-12-12 12:13:38 +0200

    Running the tests on different web browser (chrome,Firefox & Microsoft edge)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,30 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
+ //       Chrome Launch
+ //       chromedriver
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+
+//        Firefox Launch
+//        geckodriver
+//        System.setProperty("webdriver.gecko.driver","C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\geckodriver.exe");
+//        WebDriver driver = new FirefoxDriver();
+
+//       Microsoft Edge Launch
+//       msedgedriver
+         System.setProperty("webdriver.edge.driver","C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\msedgedriver.exe");
+         WebDriver driver = new EdgeDriver();
+
+
+
         driver.get("https://rahulshettyacademy.com/");
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
