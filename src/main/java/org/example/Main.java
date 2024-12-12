f017ab93a60b1f327ac51351ba25cc1c22343c5d
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2024-12-12 11:43:55 +0200

    Testing configuration web browser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,16 @@
-package org.example;public class Main {
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
     }
 }
