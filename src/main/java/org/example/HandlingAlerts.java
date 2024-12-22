bace3ac4301418aa8ac7a64a0c422f3f41bd9ffb
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2024-12-22 14:38:48 +0200

    Handling Alerts

---
+++ src/main/java/org/example/HandlingAlerts.java
@@ -0,0 +1,31 @@
+package org.example;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingAlerts {
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/AutomationPractise");
+
+        String text="Rahul";
+
+        driver.findElement(By.id("name")).sendKeys(text);
+        //handling alert with ok button only
+        driver.findElement(By.cssSelector("[id='alertbtn']")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();
+
+        //handling alert with ok and cancel buttons
+        driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();
+
+
+
+    }
+}
