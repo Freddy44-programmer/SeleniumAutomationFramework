f07635be4e4a67ca80cd3b585637162f42e06d0b
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-01-06 14:31:20 +0200

    Handling switching from Parent to child window vice versa

--- src/main/java/org/example/HandlingAjax_MouseInteractions.java
+++ src/main/java/org/example/HandlingAjax_MouseInteractions.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -13,9 +15,12 @@ public class HandlingAjax_MouseInteractions {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.com/");
         Actions a = new Actions(driver);
+        WebElement move = driver.findElement(By.cssSelector("a[id='nav-link-accountList']"));
 
+        //
+        a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
         //This moves to specific element
-        a.moveToElement(driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))).build().perform();
+        a.moveToElement(move).build().perform();
 
     }
 }
