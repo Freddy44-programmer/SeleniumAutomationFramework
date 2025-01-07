2ef3f25e70fddf001988973e91b7bff3ffc90d48
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-01-07 10:51:49 +0200

    //click on each link in the column and check if the page are opening Getting the titles of child tabs with optimized while loop

--- src/main/java/org/example/PrintingLinksCountInWebPage.java
+++ src/main/java/org/example/PrintingLinksCountInWebPage.java
@@ -6,9 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class PrintingLinksCountInWebPage {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
@@ -28,6 +31,18 @@ public class PrintingLinksCountInWebPage {
         for(int i=1; i<columndriver.findElements(By.tagName("a")).size(); i++){
            String clickonlinkTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
             columndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
-        }
+            Thread.sleep(5000L);
+        }// opens all the tabs
+
+            // Getting the titles of child tabs with optimized while loop
+            Set<String> abc = driver.getWindowHandles();
+            Iterator<String> it =abc.iterator();
+           while(it.hasNext()){
+             driver.switchTo().window(it.next());
+             System.out.println(driver.getTitle());
+           }
+
+
+
     }
 }
