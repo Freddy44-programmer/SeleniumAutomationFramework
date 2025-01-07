485ddfd5fe9c5a4c11a8ae0b40ac75c2f7348d57
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-01-07 17:06:57 +0200

    Maximizing browser window and deleting cookies Added a Code for taking screenshots

--- src/main/java/org/example/Miscelleanous_handling_takingOfScreenshorts.java
+++ src/main/java/org/example/Miscelleanous_handling_takingOfScreenshorts.java
@@ -1,11 +1,18 @@
 package org.example;
 
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Miscelleanous {
+import java.io.File;
+import java.io.IOException;
 
-    public static void main(String[] args) {
+public class Miscelleanous_handling_takingOfScreenshorts {
+
+    public static void main(String[] args) throws IOException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Freddy\\Desktop\\SeleniumAutomationFramework\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -15,6 +22,10 @@ public class Miscelleanous {
         driver.manage().deleteAllCookies();
 
         driver.get("https://google.com");
+
+        //Code for taking screenshots
+        File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(src,new File("C://screenshot.png"));
 
     }
 }
