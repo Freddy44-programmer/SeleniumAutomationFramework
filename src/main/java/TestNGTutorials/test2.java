35e2f3eeab2bcd9bde0cf40024b8d39db8001da8
freddy konyane <148207451+freddyKto@users.noreply.github.com>
2025-01-09 20:17:25 +0200

    TestNG Before and After test annotations added

--- src/main/java/TestNGTutorials/test2.java
+++ src/main/java/TestNGTutorials/test2.java
@@ -1,11 +1,28 @@
 package TestNGTutorials;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class test2 {
+
+    @AfterTest
+    public void lastExecution()
+    {
+        System.out.println("After Test annotation");
+    }
+
     @Test
     public void demo3()
     {
         System.out.println("Good day");
     }
+
+
+    @BeforeTest
+    public void prerequiste()
+    {
+        System.out.println("Before Test annotation");
+    }
+
 }
